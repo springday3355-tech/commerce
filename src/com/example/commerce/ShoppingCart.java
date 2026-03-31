4ab459239ebe7c803c237de030a6ee5f0dbe09ed
송이 <springday3355@gmail.com>
2026-03-31 19:23:45 +0900

    장바구니 출력 및 금액 계산 / 주문 및 장바구니 초기화

--- src/com/example/commerce/ShoppingCart.java
+++ src/com/example/commerce/ShoppingCart.java
@@ -1,21 +1,19 @@
 package com.example.commerce;
-
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class ShoppingCart {
-
     // 1. 속성 / 물건들을 담을 실제 바구니
-    private List<CartItem> items;
+    private static List<CartItem> items;
 
     // 2. 생성자 / 초기화
     public ShoppingCart(){
         this.items = new ArrayList<>();
     }
     // 3&4 기능
-    // 3. 메서드 1 / - 상품추가
-    public void addProduct(Product product, int quantity){
+    // 3. 메서드 1 / 상품추가
+    public static void addProduct(Product product, int quantity){
+        // 재고 줄이라고 명령
+        product.reduceStock(quantity);
         // 새로운 장바구니 항목(CartItem) 생성
         CartItem newItem = new CartItem(product.getName(), product.getPrice(), quantity , product);
         items.add(newItem); // 바구니 리스트에 추가
@@ -28,11 +26,25 @@ public class ShoppingCart {
         System.out.println("--- 장바구니 목록 ---");
 
         for(CartItem item : items) {
-            System.out.println(item.getName() + " | " + item.getQuantity() + "개"); // 2. 바구니에 든 물건들 하나씩 보여주기
+            System.out.println(item.getName() + " | " + item.getPrice() + " 원 | " + item.getQuantity() + "개"); // 2. 바구니에 든 물건들 하나씩 보여주기
             total += (item.getPrice() * item.getQuantity()); // 가격 * 수량 계산해서 합계에 계속 더하기
+            // (가격 * 수량)을 계산해서 총액에 누적해서 더함 /  total += 의미: total = total + (새로운 값)의 줄임말
+            total += (item.getPrice() * item.getQuantity());
         }
         // 4. 마지막에 총 금액 출력
+        System.out.println("----------------");
         System.out.println("총 결제 금액: " + total + "원");
     }
+    public void order(){
+        if(items.isEmpty()){// 장바구니 비어있는지 확인
+            System.out.println("장바구니가 비어 있어 주문할 수 없습니다.");
+            return;
+        }
+        // 주문 완료 메세지 출력
+        System.out.println("주문이 완료되었습니다. 이용해 주셔서 감사합니다.");
+        // 장바구니 비우기
+        items.clear();
+    }
+
 
 }
